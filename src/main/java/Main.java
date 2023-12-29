d6d7cdf7965546431863b4ee36c742d663c8ba88
trungvuive <trung150502@gmail.com>
2023-12-29 08:36:07 +0700

    passed stage 1 officially

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,31 @@ import java.net.DatagramSocket;
 
 public class Main {
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    int portNumber = 2053;
+    try(DatagramSocket serverSocket = new DatagramSocket(portNumber)) {
+
+      while(true) {
+
+        final byte[] buf = new byte[512];
+
+        final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+        serverSocket.receive(packet);
+
+        System.out.println("Received data");
+    
+        final byte[] bufResponse = new byte[512];
+
+        final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
+
+        serverSocket.send(packetResponse);
+
+      }
+
+    } catch (IOException e) {
+
+        System.out.println("IOException: " + e.getMessage());
+
+    }
   }
 }
