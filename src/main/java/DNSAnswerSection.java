315ccc1ef115cec5d4103bef23bd45c58dd9113e
trungvuive <trung150502@gmail.com>
2023-12-30 15:51:29 +0700

    minor rewriting code

--- src/main/java/DNSAnswerSection.java
+++ src/main/java/DNSAnswerSection.java
@@ -1,5 +1,4 @@
 import java.util.List;
-
 public record DNSAnswerSection(List<DNSRecord> records) {
     public record DNSRecord(String name, DNSMessage.Type dataType, DNSMessage.ClassType dataClass, int ttl, byte[] data) {}
 }
--- src/main/java/DNSQuestionSection.java
+++ src/main/java/DNSQuestionSection.java
@@ -1,5 +1,4 @@
 import java.util.List;
-
 public record DNSQuestionSection(List<DNSQuestion> questions) {
     public record DNSQuestion(String labels, DNSMessage.Type type, DNSMessage.ClassType classType) {}
 }
